38042d67a039ce212a8ef57a332d0ef86985003b
Nguyen Duy <khangduybk@gmail.com>
2016-04-28 15:30:36 +0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name:'Duy',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Duy',
+// 	// 	age: 24
+// 	// };
+// 	obi.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradesArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
+
